ef538151ab9745ff8fd3c0661d824f53e75705eb
Julien <ju.collet@gmail.com>
2017-10-25 17:47:42 +0000

    initial commit

---
+++ bundle.25dfb5eef7f256aa3be9.js
@@ -0,0 +1,29 @@
+webpackJsonp([1],{
+
+/***/ 15:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var _react = __webpack_require__(3);
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactDom = __webpack_require__(9);
+
+var _reactDom2 = _interopRequireDefault(_reactDom);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var root = document.getElementById('root');
+
+_reactDom2.default.render(_react2.default.createElement(
+  "h1",
+  null,
+  "Coucou"
+), root);
+
+/***/ })
+
+},[15]);
