c6efe1b2aeb6eeb2eefd2f8fe1d12b765546572d
Julien <ju.collet@gmail.com>
2017-12-05 21:22:02 +0100

    Add FP app utils tests

--- src/components/Select/Select.test.js
+++ src/components/Select/Select.test.js
@@ -27,21 +27,6 @@ function setup(customProps){
 
 describe('<Select/>', function(){
   
-  it('should have a class "Select"', function(){
-    const { wrapper } = setup();
-    expect(wrapper.first().hasClass('Select')).to.equal(true);
-  });
-  
-  it('should contains as many options as passed to the element', () => {
-    const selectOptions = [0,1,2,3,4];
-    const { option } = setup({selectOptions});
-    expect(option).to.have.length(selectOptions.length);
-  });
-  
-  it('should send the event back to parent on change', () => {
-    const { wrapper, mockOnChange } = setup({selectOptions : [0,1,2,3,4]});
-    wrapper.find('select').simulate('change');
-    expect(mockOnChange.calledOnce).to.equal(true);
-  });
+
   
 });
--- src/containers/app.test.js
+++ src/containers/app.test.js
@@ -1,8 +1,91 @@
 import React from 'react';
 import { mount, shallow } from 'enzyme';
+import { asyncPipe, Box, objectMap, pipe } from "./app.utils";
 import {expect} from 'chai';
 
 import App from "./app";
+
+describe('App utils', function(){
+  describe('async pipe', function(){
+    const asyncAdd2 = x => new Promise((resolve, reject) => {
+      setTimeout(function(){
+        resolve(x + 2);
+      }, 200);
+    });
+    const asyncAddPx = x => new Promise((resolve, reject) => {
+      setTimeout(function(){
+        resolve(x + "px");
+      }, 200);
+    });
+    
+    const asyncPipeMock = asyncPipe(asyncAdd2, asyncAddPx);
+    
+    it('should return the correct value', function(){
+      asyncPipeMock(2).then(function(res){
+        expect(res).to.equal('4px');
+      });
+    });
+
+    it('should not return a result before 400ms', function(){
+      const timer = Date.now();
+      asyncPipeMock(4).then(function(res){
+        const timer2 = Date.now();
+        expect (timer2 - timer > 398).to.equal(true);
+      });
+    });
+
+  });
+
+  describe('pipe', function(){
+    const add2 = x => x + 2;
+    const addPx = x => x + "px";
+    const toUpperCase = x => x.toUpperCase();
+    const pipeMock = pipe(add2, addPx, toUpperCase);
+
+    it('should return the correct result', function(){
+      expect(pipeMock(2)).to.equal('4PX');
+    });
+
+  });
+
+  describe('box', function(){
+    const boxMock = x => 
+      Box(x)
+        .map(x => x + 2)
+        .map(x => x + "px")
+        .fold(x => x.toUpperCase());
+
+    it('should fold the correct result', function(){
+      expect(boxMock(2)).to.equal('4PX');
+    });
+
+  });
+
+  describe('objectMap', function(){
+    const dataMock = {
+      x : 200,
+      y : 300,
+      z : 400,
+      text : "coucou"
+    };
+
+    const expectedResult = {
+      x : 400,
+      y : 600,
+      z : 400,
+      text : "coucou"
+    };
+
+    const keysToMap = ['x', 'y'];
+    const multiplyBy2 = x => x * 2;
+
+    it('should return the correct result', function(){
+      expect(objectMap(dataMock, keysToMap, multiplyBy2)).to.eql(expectedResult);
+    });
+
+  });
+
+});
 
 describe('<App/>', function () {
 
@@ -21,11 +104,6 @@ describe('<App/>', function () {
     };
   
   };
-
-  it('should have a class"App"', function(){
-    const { wrapper } = setup();
-    expect(wrapper.first().hasClass('App')).to.equal(true);
-  });
   
   it('should not display the preview element if user is not signed in', function(){
     const { wrapper } = setup({isSignedIn : false});
